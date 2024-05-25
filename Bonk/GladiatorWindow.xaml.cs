e23ba8045d3a3272da056393119191f84cf8102c
lundst1 <rickard.lundstedt@gmail.com>
2024-05-25 13:17:37 +0200

    Add interaction logic to gladiator window and main window

--- Bonk/GladiatorWindow.xaml.cs
+++ Bonk/GladiatorWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +20,114 @@ namespace Bonk
     /// </summary>
     public partial class GladiatorWindow : Window
     {
+        //Private integer variable for remaining ability scores.
+        private int remainingScore = 20;
+        private int? strengthScore = null;
+        private int? agilityScore = null;
+        private int? intelligenceScore = null;
+        private int? constitutionScore = null;
+        //Private variable for instance of class Gladiator.
+        private Gladiator gladiator;
+        /// <summary>
+        /// Public property for private variable gladiator.
+        /// Read access.
+        /// </summary>
+        public Gladiator Gladiator
+        {
+            get { return gladiator; }
+        }
+
         public GladiatorWindow()
         {
             InitializeComponent();
+            InitializeGUI();
+        }
+        private void InitializeGUI()
+        {
+            lblRemainingAbilityScoreValue.Content = remainingScore.ToString();
+        }
+        private bool UpdateRemainingScores(int value)
+        {
+            bool validRemainingScores = true;
+
+            remainingScore -= value;
+            
+            if (remainingScore < 0)
+            {
+                validRemainingScores = false;
+            }
+            else
+            {
+                lblRemainingAbilityScoreValue.Content = remainingScore.ToString();
+            }
+            return (validRemainingScores);
+        }
+        private int CalculateDifference(int? value1, int? value2)
+        {
+            return ((value1 ?? default(int)) - (value2 ?? default(int)));
+        }
+        private void upDownStrength_ValueChanged(object sender, EventArgs e)
+        {
+            int difference = CalculateDifference(upDownStrength.Value, strengthScore);
+            strengthScore = upDownStrength.Value;
+            UpdateRemainingScores(difference); //TODO check that update is valid
+        }
+        private void upDownAgility_ValueChanged(object sender, EventArgs e)
+        {
+            int difference = CalculateDifference(upDownAgility.Value, agilityScore);
+            agilityScore = upDownAgility.Value;
+            UpdateRemainingScores(difference);
+        }
+        private void upDownIntelligence_ValueChanged(object sender, EventArgs e)
+        {
+            int difference = CalculateDifference(upDownIntelligence.Value, intelligenceScore);
+            intelligenceScore = upDownIntelligence.Value;
+            UpdateRemainingScores(difference);
+        }
+        private void upDownConstitution_ValueChanged(object sender, EventArgs e)
+        {
+            int difference = CalculateDifference(upDownConstitution.Value, constitutionScore);
+            constitutionScore = upDownConstitution.Value;
+            UpdateRemainingScores(difference);
+        }
+        private bool validateAbilityScore(int? nullableScore, out int score)
+        {
+            bool valid = false;
+            if (nullableScore.HasValue)
+            {
+                score = nullableScore.Value;
+                valid = true;
+            }
+            else
+            {
+                score = 0;
+            }
+            return valid;
+        } 
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+
+            bool scoresValid = true;
+            scoresValid = validateAbilityScore(strengthScore, out int strength);
+            scoresValid = validateAbilityScore(agilityScore, out int agility);
+            scoresValid = validateAbilityScore(intelligenceScore, out int intelligence);
+            scoresValid = validateAbilityScore(constitutionScore, out int constitution);
+
+            if (scoresValid)
+            {
+                this.DialogResult = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please fill in a value for all scores");
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- Bonk/MainWindow.xaml.cs
+++ Bonk/MainWindow.xaml.cs
@@ -16,9 +16,42 @@ namespace Bonk
     /// </summary>
     public partial class MainWindow : Window
     {
+        Arena arena;
         public MainWindow()
         {
             InitializeComponent();
+            arena = new Arena(lstArena);
+        }
+        public void UpdateGUI()
+        {
+
+        }
+        private void btnCreateGladiator_Click(object sender, RoutedEventArgs e)
+        {
+            Gladiator gladiator;
+
+            GladiatorWindow gladiatorWindow = new GladiatorWindow();
+            gladiatorWindow.ShowDialog();
+
+            bool? result = gladiatorWindow.DialogResult;
+
+            if (result == true)
+            {
+
+
+               //arena.AddGladiator(gladiator);
+            }
+
+        }
+
+        private void btnEditGladiator_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
